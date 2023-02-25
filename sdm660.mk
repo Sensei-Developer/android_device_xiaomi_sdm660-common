ff3948a2527f4a52fc340e856e4538bc9c0d1c5d
Sensei-Developer <shoppingplay32@gmail.com>
2023-02-25 18:33:41 +0300

    sdm660-common: test

--- sdm660.mk
+++ sdm660.mk
@@ -158,8 +158,6 @@ PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0.vendor \
     android.hardware.graphics.allocator@2.0-impl:64 \
     android.hardware.graphics.allocator@2.0-service \
-    android.hardware.graphics.allocator@3.0-impl \
-    android.hardware.graphics.allocator@4.0-impl \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
@@ -184,8 +182,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.3.vendor \
-    android.hardware.drm@1.3-service.clearkey
+    android.hardware.drm@1.3.vendor
 
 # FM
 ifeq ($(BOARD_HAVE_QCOM_FM),true)
